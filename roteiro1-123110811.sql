602d89faf7c149dc91b203fe91c1c4f9bb5c9225
Heitor de Souza Alves <heitor.souza.alves@ccc.ufcg.edu.br>
2024-12-04 15:58:11 -0300

    Add files via upload

--- roteiro1-123110811.sql
+++ roteiro1-123110811.sql
@@ -1,60 +1,65 @@
 -- 2
 
 CREATE TABLE automovel (
-placa char(7),
-chassi char(17),
-marca varchar,
-modelo varchar);
+placa CHAR(7),
+chassi CHAR(17),
+marca VARCHAR,
+modelo VARCHAR);
 
 CREATE TABLE segurado (
-nome varchar,
-cpf char(11));
+cpf CHAR(11),
+nome VARCHAR);
+
 
 CREATE TABLE perito (
-nome varchar,
-id int,
-automovel_atual char(7),
-id_oficina int);
+id SERIAL,
+nome VARCHAR,
+automovel_atual CHAR(7),
+id_oficina INT);
 
 CREATE TABLE oficina(
-id int,
-nome varchar,
-bairro varchar,
-cidade varchar,
-estado varchar);
+id SERIAL,
+nome VARCHAR,
+bairro VARCHAR,
+cidade VARCHAR,
+estado VARCHAR);
 
 CREATE TABLE seguro (
+id SERIAL,
 dia_da_assinatura date,
-comprador char(11),
-segurado char(11),
-placa char(7));
+comprador CHAR(11),
+segurado CHAR(11),
+placa CHAR(7));
 
 CREATE TABLE sinistro (
-automovel char(7),
-momento_do_acidente timestamp,
-proprietario char(11),
-tipo_de_sinistro varchar,
-oficina_de_destino int);
+id SERIAL,
+automovel CHAR(7),
+momento_do_acidente TIMESTAMP,
+proprietario CHAR(11),
+tipo_de_sinistro VARCHAR,
+oficina_de_destino INT);
 
 CREATE TABLE pericia (
-id int,
-automovel char(7),
-perito int,
-proprietario char(7),
-dano varchar);
+id SERIAL,
+automovel CHAR(7),
+perito INT,
+proprietario CHAR(7),
+dano VARCHAR);
 
 CREATE TABLE reparo (
-id int,
-automovel char(7),
-proprietario char(11),
-valor numeric,
-data_inicio date,
-data_fim date);
+id SERIAL,
+automovel CHAR(7),
+proprietario CHAR(11),
+valor NUMERIC,
+data_inicio DATE,
+data_fim DATE);
 
 -- 3
 ALTER TABLE automovel ADD PRIMARY KEY (placa);
 ALTER TABLE segurado ADD PRIMARY KEY (cpf);
 ALTER TABLE perito ADD PRIMARY KEY (id);
 ALTER TABLE oficina ADD PRIMARY KEY (id);
+ALTER TABLE seguro ADD PRIMARY KEY (id);
+ALTER TABLE sinistro ADD PRIMARY KEY (id);
 ALTER TABLE pericia ADD PRIMARY KEY (id);
 ALTER TABLE reparo ADD PRIMARY KEY (id);
