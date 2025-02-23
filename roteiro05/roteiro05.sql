c214e9f2ed600fc4d123d177fc915c210dea4286
Heitor de Souza Alves <heitor.souza.alves@ccc.ufcg.edu.br>
2025-02-22 22:44:28 -0300

    atualização roteiro05

--- roteiro05/roteiro05.sql
+++ roteiro05/roteiro05.sql
@@ -13,10 +13,11 @@ SELECT s.fname nome_supervisor, COUNT(e.ssn) qtd_supervisionados FROM employee s
 --5
 SELECT s.fname nome_supervisor, COUNT(e.ssn) qtd_supervisionados FROM employee s JOIN employee e ON s.ssn = e.superssn GROUP BY nome_supervisor ORDER BY qtd_supervisionados;
 
---6 TODO
-SELECT MIN(qtd_func) qtd FROM (SELECT COUNT(essn) qtd_func FROM works_on GROUP BY pno);
+--6
+SELECT MIN(qtd_func) qtd FROM (SELECT COUNT(essn) qtd_func FROM works_on GROUP BY pno) AS foo;
 
---7 TODO
+--7
+SELECT p.pnumber AS num_projeto, COUNT(w.essn) AS qtd_func FROM project p JOIN works_on w ON p.pnumber = w.pno GROUP BY p.pnumber HAVING COUNT(w.essn) = (SELECT MIN(func_count) FROM (SELECT COUNT(essn) AS func_count FROM works_on GROUP BY pno) AS foo);
 
 --8
 SELECT w.pno num_proj, AVG(e.salary) media_sal FROM works_on w JOIN employee e ON e.ssn = w.essn GROUP BY num_proj;
@@ -24,7 +25,8 @@ SELECT w.pno num_proj, AVG(e.salary) media_sal FROM works_on w JOIN employee e O
 --9
 SELECT w.pno proj_num, p.pname proj_nome, AVG(e.salary) media_sal FROM works_on w JOIN employee e ON e.ssn = w.essn JOIN project p ON w.pno = p.pnumber GROUP BY proj_num, proj_nome;
 
---10 TODO
+--10
+SELECT fname, salary FROM employee WHERE salary > (SELECT MAX(salary) FROM employee JOIN works_on ON ssn=essn WHERE pno=92) AND NOT EXISTS (SELECT essn FROM works_on WHERE pno = 92 AND essn = ssn);
 
 --11
 SELECT e.ssn, COUNT(w.pno) qtd_proj FROM employee e LEFT JOIN works_on w ON e.ssn = w.essn GROUP BY ssn;
@@ -32,11 +34,13 @@ SELECT e.ssn, COUNT(w.pno) qtd_proj FROM employee e LEFT JOIN works_on w ON e.ss
 --12
 SELECT w.pno num_proj, COUNT(e.ssn) qtd_func FROM works_on w RIGHT JOIN employee e ON e.ssn = w.essn GROUP BY num_proj HAVING COUNT(e.ssn) < 5;
 
---13 TODO
-SELECT fname FROM()
+--13
+SELECT fname FROM employee e WHERE EXISTS (SELECT essn FROM dependent WHERE e.ssn = essn) AND EXISTS (SELECT plocation FROM project p WHERE plocation = 'Sugarland' AND EXISTS (SELECT pno FROM works_on WHERE pno=p.pnumber and essn=e.ssn));
 
---14 TODO
-SELECT dname FROM department WHERE dnumber NOT IN (SELECT dnum FROM project);
+--14
+SELECT dname FROM department WHERE NOT EXISTS (SELECT dnum FROM project WHERE dnum = dnumber);
 
---15 TODO
-SELECT  fname, lname FROM employee e JOIN works_on w ON e.ssn = 
+--15
+SELECT  fname, lname FROM employee e JOIN works_on w ON w.essn = e.ssn WHERE e.ssn='123456789' AND EXISTS (SELECT essn FROM works_on wo WHERE w.pno=wo.pno and wo.essn='123456789') GROUP BY fname,lname;
+
+--16 TODO
